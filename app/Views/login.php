d4880075000815fc6a4c8396e349145c623972ce
n4days <alirsyhad.ramadhan49@gmail.com>
2023-07-07 11:22:12 +0700

    Berhasil membuat CSS Flex Fluid. Akan tetapi <br> jadi tidak berfungsi, mungkin karena //wrap

--- app/Views/login.php
+++ app/Views/login.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <form method="post" action="/login">
-        <input type="email" name="email"><br>
-        <input type="password" name="pass"><br>
-        <button type="submit">Login</button>
+    <form method="post" action="/login" style="display:flex;flex-wrap:wrap;">
+        <input type="email" name="email" style="flex:1 0 140px;">
+        <input type="password" name="pass" style="flex:1 0 140px;">
+        <button type="submit" style="flex:1 0 140px;">Login</button>
     </form>
 </body>
 </html>
